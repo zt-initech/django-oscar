afe06300044ec69438f1d3b224063287e7181096
David Winterbottom <david.winterbottom@gmail.com>
2013-12-16 15:11:48 +0000

    Adjust update script for new uWSGI set-up

--- sites/sandbox/update_latest.sh
+++ sites/sandbox/update_latest.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cd /var/www/oscar/builds/sandbox
+cd /var/www/oscar/builds/latest
 git pull --ff-only 2> /dev/null
 [ $? -gt 0 ] && echo "Git pull failed" >&2 && exit 1
 
 # Update any dependencies
-source ../../virtualenvs/sandbox/bin/activate
+source ../../virtualenvs/latest/bin/activate
 python setup.py develop
 pip install -r requirements.txt
 
@@ -16,17 +16,16 @@ cd sites/sandbox
 ./manage.py loaddata ../_fixtures/promotions.json
 ./manage.py thumbnail clear
 ./manage.py rebuild_index --noinput
-chown -R www-data:www-data whoosh_index
 
 # Re-compile python code
-touch deploy/wsgi/sandbox.wsgi
+touch deploy/wsgi/latest
 
 # Copy down server config files
-cp deploy/nginx/sandbox.conf /etc/nginx/sites-enabled/sandbox.oscar.tangentlabs.co.uk
+cp deploy/nginx/latest.conf /etc/nginx/sites-enabled/latest.oscarcommerce.com
 /etc/init.d/nginx configtest 2> /dev/null && /etc/init.d/nginx force-reload 2> /dev/null
 
-cp deploy/apache2/sandbox.conf /etc/apache2/sites-enabled/sandbox.oscar.tangentlabs.co.uk
-/etc/init.d/apache2 reload > /dev/null
+cp deploy/supervisord/latest.conf /etc/supervisord/conf.d/oscar-latest.conf
+touch /var/www/oscar/builds/latest/deploy/wsgi/prod.wsgi
 
 # Copy down cronjob file
-cp deploy/cron.d/oscar /etc/cron.d/oscar-sandbox
+cp deploy/cron.d/oscar /etc/cron.d/oscar-latest

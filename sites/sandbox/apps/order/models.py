6c604442379bf486daed45d415bbde895f77ab34
David Winterbottom <david.winterbottom@gmail.com>
2014-11-05 22:02:07 +0000

    Adjust postcode validation for French shipping addresses

--- sites/sandbox/apps/order/models.py
+++ sites/sandbox/apps/order/models.py
@@ -1,1 +1,10 @@
+from oscar.apps.address import abstract_models
+
+
+class ShippingAddress(abstract_models.AbstractShippingAddress):
+    # Adjust the regex for France
+    POSTCODES_REGEX = abstract_models.AbstractAddress.POSTCODES_REGEX.copy()
+    POSTCODES_REGEX['FR'] = r'^[0-9]{4}$'
+
+
 from oscar.apps.order.models import *  # noqa
